afaba58e6a3a11d362fcd7aa4bd5be7affea9824
Gabriel Albuquerque <114111654+gabalbcos@users.noreply.github.com>
2023-03-21 16:42:25 -0300

    Add organize code

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,6 @@
 let color = 'black';
 let gridCellNumb = '';
+
 //buttons
 const btnPickColor = document.querySelector('.btn-1');
 const btnClear = document.querySelector('.btn-2');
@@ -7,20 +8,20 @@ const colorButton = document.querySelector('.btn-1');
 
 //grid maker
 function makeGrid(size){
-let gridArea = document.querySelector('.device-body__screen');
-let cell = gridArea.querySelectorAll('div')
-cell.forEach((div) => div.remove());
-gridArea.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-gridArea.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-gridCellNumb = size;
-console.log(gridCellNumb)
-//adding cells
-let area = size * size;
-for(let i = 0; i < area; i++){
-    let cell = document.createElement('div');
-    cell.addEventListener('mouseover', colorCell);
-    cell.style.backgroundColor = 'grey';
-    gridArea.insertAdjacentElement('beforeend', cell)
+    let gridArea = document.querySelector('.device-body__screen');
+    let cell = gridArea.querySelectorAll('div')
+    cell.forEach((div) => div.remove());
+    gridArea.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    gridArea.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    gridCellNumb = size;
+
+    //adding cells
+    let area = size * size;
+    for(let i = 0; i < area; i++){
+        let cell = document.createElement('div');
+        cell.addEventListener('mouseover', colorCell);
+        cell.style.backgroundColor = 'grey';
+        gridArea.insertAdjacentElement('beforeend', cell)
  }
 }
 
