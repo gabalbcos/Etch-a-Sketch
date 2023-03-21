31f0949769fbb41b4426e014c0608da3f292a385
Gabriel Albuquerque <114111654+gabalbcos@users.noreply.github.com>
2023-03-21 15:39:18 -0300

    Add js for painting squares, making grid and html to change button size

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,29 @@
+
 //buttons
 const btnPickColor = document.querySelector('.btn-1');
 const btnClear = document.querySelector('.btn-2');
-const btnSmall = document.querySelector('.small_opt');
-const btnMedium = document.querySelector('.medium_opt');
-const btnLarge = document.querySelector('.large_opt');
 
-//
+
+//grid maker
+function makeGrid(size){
+let gridArea = document.querySelector('.device-body__screen');
+let cell = gridArea.querySelectorAll('div')
+cell.forEach((div) => div.remove());
+gridArea.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+gridArea.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+//adding cells
+let area = size * size;
+for(let i = 0; i < area; i++){
+    let cell = document.createElement('div');
+    cell.addEventListener('mouseover', colorCell);
+    cell.style.backgroundColor = 'grey';
+    gridArea.insertAdjacentElement('beforeend', cell)
+ }
+}
+
+function colorCell(){
+    this.style.backgroundColor = 'black';  
+}
+
+
