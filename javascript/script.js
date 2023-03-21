f61eb09d16e9dd2e4aef0c77145bf9dfac6be898
Gabriel Albuquerque <114111654+gabalbcos@users.noreply.github.com>
2023-03-20 23:57:17 -0300

    Add initial Js buttons query

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,8 @@
-const squares = document.querySelectorAll(".container div");
+//buttons
+const btnPickColor = document.querySelector('.btn-1');
+const btnClear = document.querySelector('.btn-2');
+const btnSmall = document.querySelector('.small_opt');
+const btnMedium = document.querySelector('.medium_opt');
+const btnLarge = document.querySelector('.large_opt');
 
-squares.forEach((ev) => {
-    ev.addEventListener('mouseover', (ev) => {
-        ev.style.backgroundColor = "blue";
-    })
-})
-
-function turnBlue()
+//
