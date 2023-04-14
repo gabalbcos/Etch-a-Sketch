82c77a9f396aca703d32a97082f3e574ea26d7e1
Gabriel Albuquerque <114111654+gabalbcos@users.noreply.github.com>
2023-04-14 11:54:20 -0300

    change js code to airbnb format

--- javascript/script.js
+++ javascript/script.js
@@ -1,37 +1,32 @@
 let color = 'black';
-let gridCellNumb = '';
 
-//buttons
-const btnPickColor = document.querySelector('.btn-1');
-const btnClear = document.querySelector('.btn-2');
+// buttons
 const colorButton = document.querySelector('.btn-1');
 
-//grid maker
-function makeGrid(size){
-    let gridArea = document.querySelector('.device-body__screen');
-    let cell = gridArea.querySelectorAll('div')
-    cell.forEach((div) => div.remove());
-    gridArea.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    gridArea.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-    gridCellNumb = size;
-
-    //adding cells
-    let area = size * size;
-    for(let i = 0; i < area; i++){
-        let cell = document.createElement('div');
-        cell.addEventListener('mouseover', colorCell);
-        cell.style.backgroundColor = 'grey';
-        gridArea.insertAdjacentElement('beforeend', cell)
- }
+function changeColor() {
+  color = colorButton.value;
 }
 
-colorButton.addEventListener("input", changeColor);
-
-function colorCell(){
-    this.style.backgroundColor = color;  
+function colorCell() {
+  this.style.backgroundColor = color;
 }
 
-function changeColor(){
-    color = colorButton.value;
-    console.log(colorButton.value)
+// grid maker
+function makeGrid(size) {
+  const gridArea = document.querySelector('.device-body__screen');
+  // remove cells before creating new grid
+  const createdCell = gridArea.querySelectorAll('div');
+  createdCell.forEach((div) => div.remove());
+  gridArea.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  gridArea.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  // adding cells
+  const area = size * size;
+  for (let i = 0; i < area; i += 1) {
+    const cell = document.createElement('div');
+    cell.addEventListener('mouseover', colorCell);
+    cell.style.backgroundColor = 'grey';
+    gridArea.insertAdjacentElement('beforeend', cell);
+  }
 }
+
+colorButton.addEventListener('input', changeColor);
